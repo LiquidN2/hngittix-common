a562aae25990d55872526ddb28aad69fdc740fa9
Hugh Nguyen <hhnguyen255@gmail.com>
2022-04-04 17:26:45 +0700

    Updates

---
+++ src/events/ticket-deleted-event.ts
@@ -0,0 +1,11 @@
+import { Subjects } from './subjects';
+
+export interface TicketDeletedEvent {
+  subject: Subjects.TicketDelete;
+  data: {
+    id: string;
+    title: string;
+    price: number;
+    userId: string;
+  };
+}
